3b27c2d3bcac30e80caea44d2185e9284226723f
iiiked <igor@topfliteagency.com>
2019-01-17 19:52:23 +0100

    transcribe

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function rnaTranscriber(sequence) {
-
+  if (sequence === undefined || !sequence.length) return '';
+  if (!/^[CGATU]+$/.test(sequence)) throw new TypeError('Invalid input');
+  const dict = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U'
+  };
+  return sequence.replace(/\w/g, c => dict[c]);
 }
 
 module.exports = rnaTranscriber;
